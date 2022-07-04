60a60d025bf8c4e2aa09ce7475650e47f9b42dee
Elena <rozetta.95@mail.ru>
2022-07-05 01:14:08 +0300

    create vue projectt

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+#app {
+  width: 1440px;
+  margin: 0 auto;
+  padding: 0 24px;
+  font-family: 'Source Sans Pro', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  color: #2c3e50;
+}
+</style>
